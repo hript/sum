13b0e6f0b6e7847b9cfe1ca91dc9087a41a8ee41
Artem Arhipov <artemiy3893@gmail.com>
2020-09-18 09:31:27 +0300

    Finding string with max size

--- Source.cpp
+++ Source.cpp
@@ -3,48 +3,6 @@
 
 using namespace std;
 
-void reverseString(string& s) {
-
-	for (long long int i = 0; i < s.size() / 2; i++) {
-		swap(s[i], s[s.size() - 1 - i]);
-	}
-
-}
-
-void pushZeros(string& minString, const string& maxString) {
-
-	int i = minString.size();
-
-	while (i < maxString.size()) {
-		minString += '0';
-		i += 1;
-	}
-
-}
-
-string sum(const string& minString, const string& maxString) {
-	string result;
-
-	char dec = '0';
-	char units = '0';
-
-	for (long long int i = 0; i < maxString.size(); i++) {
-
-		int x = dec - '0' + maxString[i] - '0' + minString[i] - '0';
-
-		dec = x / 10 + '0';
-		units = x % 10 + '0';
-
-		result += units;
-
-	}
-	if (dec != '0') {
-		result += dec;
-	}
-
-	return result;
-}
-
 int main() {
 	string first;
 	string second;
@@ -65,21 +23,6 @@ int main() {
 		maxString = second;
 		minString = first;
 	}
-
-	reverseString(maxString);
-	reverseString(minString);
-
-	pushZeros(minString, maxString);
-
-	result = sum(minString, maxString);
-
-	reverseString(result);
-
-
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i];
-	}
-	cout << endl;
 
 
 	return 0;
