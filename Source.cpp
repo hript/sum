27c0d9251b96d118a510248368a5c320e9796e29
Artem Arhipov <artemiy3893@gmail.com>
2020-09-21 18:39:10 +0300

    Konec

--- Source.cpp
+++ Source.cpp
@@ -3,83 +3,52 @@
 
 using namespace std;
 
-void reverseString(string& s) {
+void reverse(string& s) {
 
-	for (long long int i = 0; i < s.size() / 2; i++) {
-		swap(s[i], s[s.size() - 1 - i]);
+	for (int i = 0; i < s.size() / 2; i++) {
+		swap(s[i], s[s.size() - i - 1]);
 	}
 
 }
 
-void pushZeros(string& minString, const string& maxString) {
+string result(const string& first, const string& second) {
 
-	int i = minString.size();
+	int maxSize = first.size() > second.size() ? first.size() : second.size();
+	int minSize = first.size() > second.size() ? second.size() : first.size();
+	string maxString = first.size() > second.size() ? first : second;
+	string minString = first.size() < second.size() ? first : second;
+	string res;
 
-	while (i < maxString.size()) {
-		minString += '0';
-		i += 1;
+	int dec = 0;
+	int ans;
+
+	for (int i = 0; i < maxSize; i++) {
+		if (i <= minSize - 1) {
+			ans = maxString[maxSize - i - 1] - '0' + minString[minSize - i - 1] - '0' + dec;
+		}
+		else {
+			ans = maxString[maxSize - i - 1] - '0' + dec;
+		}
+		dec = ans / 10;
+		res += ans % 10 + '0';
 	}
 
-}
+	if (dec > 0) res += dec + '0';
 
-string sum(const string& minString, const string& maxString) {
-	string result;
+	reverse(res);
 
-	char dec = '0';
-	char units = '0';
-
-	for (long long int i = 0; i < maxString.size(); i++) {
-
-		int x = dec - '0' + maxString[i] - '0' + minString[i] - '0';
-
-		dec = x / 10 + '0';
-		units = x % 10 + '0';
-
-		result += units;
-
-	}
-	if (dec != '0') {
-		result += dec;
-	}
-
-	return result;
+	return res;
 }
 
 int main() {
-	string first;
-	string second;
-	string result;
-	cout << "First number" << endl;
-	cin >> first;
 
-	cout << "Second number" << endl;
-	cin >> second;
+	string first, second;
 
-	string maxString;
-	string minString;
-	if (first.size() > second.size()) {
-		maxString = first;
-		minString = second;
-	}
-	else {
-		maxString = second;
-		minString = first;
-	}
+	cin >> first >> second;
 
-	reverseString(maxString);
-	reverseString(minString);
-
-	pushZeros(minString, maxString);
-
-	result = sum(minString, maxString);
-
-	reverseString(result);
+	cout << result(first, second);
 
 
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i];
-	}
-	cout << endl;
 
 	return 0;
 }
